201f4d71f50b4a097d43b37339bb255009dbf54c
Jeong WooSung <wjddntjd119@naver.com>
2022-12-23 13:54:47 +0900

    2장

---
+++ js/2-1.js
@@ -0,0 +1,17 @@
+let number = 4+10;
+console.log(number);
+
+number = 4+ "10";
+console.log(number);
+
+number = parseInt(10+0.5);
+console.log(number);
+
+number = "10"+0.5;
+console.log(number);
+
+number = 10+0.5;
+console.log(number);
+
+number = parseFloat("10"+0.5)
+console.log(number);
---
+++ js/2-2.js
@@ -0,0 +1,4 @@
+let  length = prompt("인치값을 입력하세요");
+let inch = length;
+length *= 2.54;
+alert(`${inch}인치는 ${length}cm 입니다`)
